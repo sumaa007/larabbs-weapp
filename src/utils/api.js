bf52dc0b08adfeaf543665eb5199f68127780ef5
sumaa007 <liujiezr@foxmail>
2018-08-16 18:40:54 +0800

    :bug: 未解决

--- src/utils/api.js
+++ src/utils/api.js
@@ -104,6 +104,29 @@ const getToken = async (options) => {
   return accessToken
 }
 
+const updateFile = async (options = {}) => {
+    // 显示loading
+  wepy.showLoading({title: '上传中'})
+
+    // 获取 token
+  let accessToken = await getToken()
+
+    // 拼接url
+  options.url = host + '/' + options.url
+  let header = options.header || {}
+    // 将 token 设置在 header 中
+  header.Authorization = 'Bearer ' + accessToken
+  options.header = header
+
+    // 上传文件
+  let response = await wepy.uploadFile(options)
+
+    // 隐藏 loading
+  wepy.hideLoading()
+
+  return response
+}
+
 // 带身份认证的请求
 const authRequest = async (options, showLoading = true) => {
   if (typeof options === 'string') {
@@ -147,5 +170,6 @@ export default {
   authRequest,
   refreshToken,
   login,
-  logout
+  logout,
+  updateFile
 }
